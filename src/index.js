91da9ba7acc5335d6bde2b83221ddc1409148ea2
Chris Basham <chris@bash.am>
2015-08-27 15:48:01 -0400

    Simplify getting render container

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Greeting from './components/Greeting';
 
-// Inject id from Webpack DefinePlugin globals.
-let containerId = '#' + CONTAINER_ID;
-
 React.render(
   <Greeting name="World"/>,
-  document.body.querySelector(containerId)
+  // Inject id from Webpack DefinePlugin globals.
+  document.getElementById(CONTAINER_ID)
 );
